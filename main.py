4e883ebb6490074dc5a478f8e00b2e9ebc7400c1
Sazzad <100172469+saztaz@users.noreply.github.com>
2024-07-05 09:21:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ from flask import Flask, render_template, request, session, redirect, url_for
 from flask_socketio import join_room, leave_room, send, SocketIO
 import random
 from string import ascii_uppercase
+
+# Initialize the Flask application
+app = Flask(__name__)
+# Set a secret key for session management
+app.config["SECRET_KEY"] = "hjhjsdahhds"
+# Initialize Flask-SocketIO for real-time communication
+socketio = SocketIO(app)
