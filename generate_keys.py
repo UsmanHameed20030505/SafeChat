468e62054fa2d0a87ecd263a08a889cf5b02ecaa
Sami7801 <91937487+Sami7801@users.noreply.github.com>
2024-07-07 00:45:08 +0100

    Update generate_keys.py

--- generate_keys.py
+++ generate_keys.py
@@ -2,3 +2,5 @@ from Crypto.PublicKey import RSA
 def generate_keys(username):
     # Generate a new RSA key pair
     key = RSA.generate(2048)
+    # Export the private key in PEM format
+    private_key = key.export_key()
